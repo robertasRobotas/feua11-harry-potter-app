7c3b1c2b7fec510c8683d2d286fa4478615abcd1
Robertas <r.ankudovicius@gmail.com>
2024-01-17 19:57:09 +0200

    Added main components

---
+++ components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css";
+
+const Header = () => {
+  return <div className={styles.main}>Header</div>;
+};
+
+export default Header;
---
+++ components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+import styles from "./styles.module.css";
+
+const Main = () => {
+  return (
+    <div className={styles.main}>
+      <Header />
+    </div>
+  );
+};
+
+export default Main;
